ff0fb88d85436cd0725c63f49dcc8644e180c06e
Sasha Patsel <user@Users-MacBook-Pro.local>
2019-02-19 17:31:18 -0500

    add listing model

--- destate/listings/views.py
+++ destate/listings/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(req):
-    render(req, "listings/listings.html")
+    return render(req, "listings/listings.html")
 
 def listing(req):
-    render(req, "listings/listing.html")
+    return render(req, "listings/listing.html")
 
 def search(req):
-    render(req, "listings/search.html")
+    return render(req, "listings/search.html")
