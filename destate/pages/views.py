9e507f3bed9b7f3300b34a1da9605d88b69675be
Sasha Patsel <user@Users-MBP.cable.rcn.com>
2019-02-20 13:26:21 -0500

    dynamic about page

--- destate/pages/views.py
+++ destate/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(req):
@@ -13,5 +14,15 @@ def index(req):
     return render(req, "pages/index.html", context)
 
 def about(req):
-    return render(req, "pages/about.html")
+    #get all realtors
+    realtors = Realtor.objects.order_by("-hire_date")
+
+    #get mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        "realtors": realtors,
+        "mvp_realtors": mvp_realtors
+    }
+    return render(req, "pages/about.html", context)
     
