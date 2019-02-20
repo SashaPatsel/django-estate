78f35a016ed1908a6e3123944bf9fa2ad7a9424e
Sasha Patsel <user@Users-MBP.cable.rcn.com>
2019-02-20 11:29:44 -0500

    customize listings admin

--- destate/listings/admin.py
+++ destate/listings/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Listing
 
 # Register your models here.
-admin.site.register(Listing )
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published", "price", "list_date", "realtor")
+    # clickable links
+    list_display_links = ("id", "title")
+    # Can filter by following fields
+    list_filter = ("realtor",)
+    list_editable = ("is_published",)
+    search_fields = ("title", "description", "address", "city", "state", "zipcode", "price")
+    list_per_page = 25
+admin.site.register(Listing, ListingAdmin)
