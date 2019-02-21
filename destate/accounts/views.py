d3496ab9414289eac7e7c4a2ab76ccb85fbfe655
Sasha Patsel <SPatsel@DOW-ADS.NET>
2019-02-21 15:39:35 -0500

    send error if passwords do not match

--- destate/accounts/views.py
+++ destate/accounts/views.py
@@ -4,9 +4,19 @@ from django.contrib import messages
 # Create your views here.
 def register(req):
     if req.method == "POST":
-        #Register new user
-        messages.error(req, "Testing error message")
-        return redirect("register")
+        first_name = req.POST["first_name"]
+        last_name = req.POST["last_name"]
+        username = req.POST["username"]
+        email = req.POST["email"]
+        password = req.POST["password"]
+        password2 = req.POST["password2"]
+
+        #check if passwords match
+        if password == password2:
+            return
+        else:
+            messages.error(req, "Passwords do not match")
+            return redirect("register") 
     else:
         return render(req, "accounts/register.html")
 
