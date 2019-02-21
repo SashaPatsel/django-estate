2e986ce8ad1fddff1482df6168cf7f2a3a9bbc53
Sasha Patsel <SPatsel@DOW-ADS.NET>
2019-02-21 17:29:47 -0500

    user can send inquiry

--- destate/contacts/views.py
+++ destate/contacts/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 # Create your views here.
 def contact(req):
-    return
+    if req.method == "POST":
+        listing_id = req.POST["listing_id"]
+        listing = req.POST["listing"]
+        name = req.POST["name"]
+        email = req.POST["email"]
+        phone = req.POST["phone"]
+        message = req.POST["message"]
+        user_id = req.POST["user_id"]
+        realtor_email = req.POST["realtor_email"]
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        messages.success(req, "Your request has been submitted. A realtor will get back to you soon")
+
+        return redirect("/listings/" + listing_id)
+
